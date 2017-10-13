dc8cc1994b7fc8b4cd7c73b26fb9288bfafc5c44
Silence <392163939@qq.com>
2017-10-13 21:29:33 +0800

    他们的Core模块 + 我们的测试模块
    Signed-off-by: Silence <392163939@qq.com>

---
+++ Project3/Core.cpp
@@ -0,0 +1,501 @@
+// Core.cpp : 定义 DLL 应用程序的导出函数。
+
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h> 
+#include <exception>  
+#include "Core.h"
+
+
+
+
+numberException::numberException() :exception("Exception: sudoku number error\n")
+{
+}
+
+boundException::boundException() : exception("Exception: lower or upper error\n")
+{
+}
+
+modeException::modeException() : exception("Exception: sudoku mode error\n")
+{
+}
+
+bool __stdcall Core::solve(int puzzle[81], int solution[81])
+{
+    int tag = -1;
+    DLX(puzzle, solution, tag);
+    if (tag == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+void __stdcall Core::generate(int number, int result[][81])
+{
+    int count_total = 0;
+    int nums1[9] = { 7,1,2,3,4,5,6,8,9 };
+    int nums[9] = { 0 };
+    int sudo[9][9] = { 0 };
+    int mark[9] = { 0 };
+    if (number < 1 || number>1000000)
+    {
+        throw numberException();
+        return;
+    }
+    for (int i = 0; i < 9; i++) {
+        int count = 0;
+        int move = (rand() % (9 - i) + 1);
+        for (int j = 0; j < 9; j++) {
+            if (mark[j] == 0) count++;
+            if (count == move) {
+                nums[i] = nums1[j];
+                mark[j] = 1;
+                break;
+            }
+        }
+    }
+    produce(number, nums, 0, count_total, 0, sudo, result);
+}
+
+void __stdcall Core::generate(int number, int mode, int result[][81])
+{
+    int count_total = 0;
+    int nums1[9] = { 7,1,2,3,4,5,6,8,9 };
+    int nums[9] = { 0 };
+    int sudo[9][9] = { 0 };
+    int mark[9] = { 0 };
+    if (number < 1 || number>10000)
+    {
+        throw numberException();
+        return;
+    }
+    for (int i = 0; i < 9; i++) {
+        int count = 0;
+        int move = (rand() % (9 - i) + 1);
+        for (int j = 0; j < 9; j++) {
+            if (mark[j] == 0) count++;
+            if (count == move) {
+                nums[i] = nums1[j];
+                mark[j] = 1;
+                break;
+            }
+        }
+    }
+    produce(number, nums, 0, count_total, 0, sudo, result);
+    for (int i = 0; i < number; i++)
+    {
+        blank(result[i], mode);
+    }
+}
+
+void __stdcall Core::generate(int number, int lower, int upper, bool unique, int result[][81])
+{
+    int count_total = 0;
+    int nums1[9] = { 7,1,2,3,4,5,6,8,9 };
+    int nums[9] = { 0 };
+    int sudo[9][9] = { 0 };
+    int mark[9] = { 0 };
+    if (number < 1 || number>10000)
+    {
+        throw numberException();
+        return;
+    }
+    for (int i = 0; i < 9; i++) {
+        int count = 0;
+        int move = (rand() % (9 - i) + 1);
+        for (int j = 0; j < 9; j++) {
+            if (mark[j] == 0) count++;
+            if (count == move) {
+                nums[i] = nums1[j];
+                mark[j] = 1;
+                break;
+            }
+        }
+    }
+    produce(number, nums, 0, count_total, 0, sudo, result);
+    int temp[81] = { 0 };
+    for (int i = 0; i < number; i++)
+    {
+        for (int j = 0; j < 81; j++)
+        {
+            temp[j] = result[i][j];
+        }
+        blank(temp, lower, upper, unique);
+        for (int j = 0; j < 81; j++)
+        {
+            if (temp[j] == 0)
+            {
+                result[i][j] = 0;;
+            }
+        }
+    } 
+}
+
+bool __stdcall Core::blank(int puzzle[81], int mode)
+{
+    int lower;
+    int upper;
+    bool suc = false;
+    switch (mode)
+    {
+    case 1:
+        lower = 20;
+        upper = 35;
+        break;
+    case 2:
+        lower = 36;
+        upper = 45;
+        break;
+    case 3:
+        lower = 46;
+        upper = 55;
+        break;
+    default:
+        throw modeException();
+        return false;
+    }
+    suc = blank(puzzle, lower, upper, false);
+    return suc;
+}
+
+
+bool __stdcall Core::blank(int puzzle[81], int lower, int upper, bool unique)
+{
+    int blankNum = 0;
+    int tag = -1;
+    int temp[81];
+    int buf[81];
+    if (lower < 20 || upper>55 || lower > upper)
+    {
+        throw boundException();
+        return false;
+    }
+    for (int i = 0; i < 81; ++i) {
+        buf[i] = puzzle[i];
+    }
+    blankNum = (rand() % (upper - lower + 1)) + lower;
+    do
+    {
+        for (int i = 0; i < 81; i++)
+        {
+            puzzle[i] = buf[i];
+        }
+        int j = 0;
+        for (int i = 0; i < blankNum; i++)
+        {
+            j = rand() % 81;
+            if (puzzle[j] == 0)
+            {
+                i--;
+            }
+            else
+            {
+                puzzle[j] = 0;
+            }
+        }
+        if (unique)
+        {
+            tag = 0;
+            DLX(puzzle, temp, tag);
+        }
+    } while (unique && (tag == 2));
+    return true;
+}
+
+
+Node* Core::toMatrix(int puzzle[81])
+{
+    int row_num = 0;
+    Node *head, *temp[4];
+    head = (Node*)malloc(sizeof(Node));
+    head->col = head->down = head->up = head;
+    Node *c[324];
+    for (int i = 0; i < 324; i++)
+    {
+        c[i] = (Node*)malloc(sizeof(Node));
+        c[i]->up = c[i];
+        c[i]->down = c[i];
+    }
+    head->right = c[0];
+    head->left = c[323];
+    for (int i = 0; i < 324; i++)
+    {
+        c[i]->row = 0;
+        c[i]->count = 0;
+        c[i]->col = c[i];
+        c[i]->num[0] = i;
+        if (i == 0)
+        {
+            c[i]->left = head;
+            c[i]->right = c[1];
+        }
+        else if (i == 323)
+        {
+            c[i]->left = c[322];
+            c[i]->right = head;
+        }
+        else
+        {
+            c[i]->left = c[i - 1];
+            c[i]->right = c[i + 1];
+        }
+    }
+    for (int i = 0; i < 81; i++)
+    {
+        for (int k = (puzzle[i] == 0 ? 0 : puzzle[i] - 1); k < (puzzle[i] == 0 ? 9 : puzzle[i]); k++)
+        {
+            row_num++;
+            for (int m = 0; m < 4; m++)
+            {
+                temp[m] = (Node*)malloc(sizeof(Node));
+            }
+            for (int m = 0; m < 4; m++)
+            {
+                temp[m]->row = row_num;
+                if (m == 3)
+                {
+                    temp[2]->right = temp[3];
+                    temp[3]->left = temp[2];
+                    temp[3]->right = temp[0];
+                    temp[0]->left = temp[3];
+                }
+                else if (m != 0)
+                {
+                    temp[m]->left = temp[m - 1];
+                    temp[m - 1]->right = temp[m];
+                }
+            }
+            temp[0]->col = c[i];
+            temp[1]->col = c[81 + i / 9 * 9 + k];
+            temp[2]->col = c[162 + i % 9 * 9 + k];
+            temp[3]->col = c[243 + (i / 27 * 3 + i % 9 / 3) * 9 + k];
+            for (int m = 0; m < 4; m++)
+            {
+                temp[m]->num[0] = i / 9;
+                temp[m]->num[1] = i % 9;
+                temp[m]->num[2] = k + 1;
+                temp[m]->col->count++;
+                temp[m]->up = temp[m]->col->up;
+                temp[m]->down = temp[m]->col;
+                temp[m]->col->up->down = temp[m];
+                temp[m]->col->up = temp[m];
+            }
+        }
+    }
+    return head;
+}
+
+void Core::remove(Node* c)
+{
+    c->left->right = c->right;
+    c->right->left = c->left;
+    Node *x = c->down;
+    while (x != c)
+    {
+        Node *y = x->right;
+        while (y != x)
+        {
+            y->up->down = y->down;
+            y->down->up = y->up;
+            y = y->right;
+            y->col->count--;
+        }
+        x = x->down;
+    }
+}
+
+void Core::resume(Node* c)
+{
+    c->left->right = c;
+    c->right->left = c;
+    Node *x = c->down;
+    while (x != c)
+    {
+        Node *y = x->right;
+        while (y != x)
+        {
+            y->up->down = y;
+            y->down->up = y;
+            y = y->right;
+            y->col->count++;
+        }
+        x = x->down;
+    }
+}
+
+int Core::dance(Node* head, int solution[81], int &tag)
+{
+    if (head->right == head)
+    {
+        if (tag == 0)
+        {
+            tag = 1;
+            return 0;
+        }
+        if (tag == 1)
+        {
+            tag = 2;
+            delete head;
+            return 1;
+        }
+        if (tag == -1)
+        {
+            tag = 1;
+            return 1;
+        }
+    }
+    Node *i = head->right->right;
+    Node *temp = head->right;
+    while (i != head)
+    {
+        if (i->count<temp->count)
+            temp = i;
+        i = i->right;
+    }
+    if (temp->down == temp)return 0;
+    remove(temp);
+    i = temp->down;
+    while (i != temp)
+    {
+        Node *j = i->right;
+        solution[i->num[0] * 9 + i->num[1]] = i->num[2];
+        while (j != i)
+        {
+            remove(j->col);
+            j = j->right;
+        }
+        if (dance(head, solution, tag))
+        {
+            return 1;
+        }
+        j = i->left;
+        while (j != i)
+        {
+            resume(j->col);
+            j = j->left;
+        }
+        solution[i->num[0] * 9 + i->num[1]] = 0;
+        i = i->down;
+    }
+    resume(temp);
+    return 0;
+}
+
+int Core::DLX(int puzzle[81], int solution[81], int &tag)
+{
+    Node* head = toMatrix(puzzle);
+    for (int i = 0; i < 81; i++)
+    {
+        solution[i] = 0;
+    }
+    return dance(head, solution, tag);
+}
+
+bool Core::isinraw(int num, int raw_num, int sudo[9][9])
+{//行
+    for (int i = 0; i < 9; i++)
+    {
+        if (sudo[raw_num][i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+bool Core::isincolumn(int num, int c_num, int sudo[9][9])
+{//列
+    for (int i = 0; i < 9; i++)
+    {
+        if (sudo[i][c_num] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int Core::insert(int num, int blocknum, int marked[], int sudo[9][9])
+{
+
+    int pos[9] = { 0,1,2,3,4,5,6,7,8 };
+    int x;
+    int y;
+    for (int i = 0; i < 9; i++)
+    {
+        x = (pos[i] / 3) + ((blocknum - 1) / 3) * 3;
+        y = (pos[i] % 3) + (((blocknum - 1) % 3) * 3);
+        if (sudo[x][y] != 0 || marked[pos[i]] == 1 || isinraw(num, x, sudo) || isincolumn(num, y, sudo))
+        {
+            continue;
+        }
+        else
+        {
+            sudo[x][y] = num;
+            return pos[i];
+        }
+    }
+    return -1;
+}
+
+
+void Core::out(int sudo[9][9], int result[][81], int count_total)
+{
+    int count = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            result[count_total - 1][count++] = sudo[i][j];
+        }
+    }
+}
+
+void Core::produce(int total, int nums[], int block_num, int & count_total, int count_nums, int sudo[9][9], int result[][81])
+{
+    int marked[9] = { 0 };//标记已经试过的位置
+    int new_block_num, new_count_nums;
+    int now_sudo[9][9];
+    while (true)
+    {
+        new_block_num = block_num + 1;
+        new_count_nums = count_nums;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                now_sudo[i][j] = sudo[i][j];
+            }
+        }
+        int now = insert(nums[new_count_nums], new_block_num, marked, now_sudo);
+
+        if (now < 0)
+        {
+            return;
+        }
+        else
+        {
+            marked[now] = 1;
+        }
+
+        if (new_block_num == 9)
+        {
+            if (new_count_nums < 8)
+            {
+                new_count_nums = count_nums + 1;
+                new_block_num = 0;
+            }
+            else
+            {//填写至最后一个
+                count_total++;
+                out(now_sudo, result, count_total);
+                return;
+            }
+        }
+        produce(total, nums, new_block_num, count_total, new_count_nums, now_sudo, result);
+        if (count_total == total) return;
+    }
+}
--- SudokuTest/UnitTest.cpp
+++ SudokuTest/UnitTest.cpp
@@ -1,10 +1,213 @@
 ﻿#include "stdafx.h"
-#include "../Sudoku-2/Core.h"
+#include "../Project3/Core.h"
+#include <iostream>
 
 using namespace System;
 using namespace System::Text;
 using namespace System::Collections::Generic;
 using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace std;
+
+const int MAXROW = 9 * 9 * 9;
+const int MAXCOL = 1 + 9 * 9 * 4;
+const int NUM = MAXCOL + MAXROW * 4;	// 总元素个数,  
+                                        // 第一个为Head元素,接着9*9*4个为列标元素,最后9*9*9*4个为“1”元素
+int g_left[NUM], g_right[NUM], g_up[NUM], g_down[NUM];    // 每个元素的4个方向分量（相当于链表中的箭头）
+int g_col[NUM];        // 记录每个元素的列标元素
+int g_row[NUM];        // 记录每个元素所在的01矩阵行数
+int g_size[MAXCOL];    // 记录每列的“1”元素个数
+int g_head[MAXROW];    // 记录每行第一个“1”元素
+int g_no;				// 元素编号
+
+void init()
+{
+    memset(g_left, 0, sizeof(g_left));
+    memset(g_right, 0, sizeof(g_right));
+    memset(g_up, 0, sizeof(g_up));
+    memset(g_down, 0, sizeof(g_down));
+    memset(g_col, 0, sizeof(g_col));
+    memset(g_row, 0, sizeof(g_row));
+    memset(g_size, 0, sizeof(g_size));
+    memset(g_head, 0, sizeof(g_head));
+}
+
+void insertNode(int col, int no)     /* 将no号元素添加到col列里 */
+{
+    /* 在元素col和Down[col]之间插入no */
+    g_up[g_down[col]] = no;
+    g_down[no] = g_down[col];
+    g_up[no] = col;
+    g_down[col] = no;
+    g_size[col]++;
+    g_col[no] = col;
+}
+
+void remove(int col)
+{
+    /* 移除col列标元素 */
+    g_left[g_right[col]] = g_left[col];
+    g_right[g_left[col]] = g_right[col];
+    /* 移除col列“1”元素与其同行元素 */
+    for (int i = g_down[col]; i != col; i = g_down[i])   // 向下删
+    {
+        for (int j = g_right[i]; j != i; j = g_right[j]) // 向右删
+        {
+            g_up[g_down[j]] = g_up[j];
+            g_down[g_up[j]] = g_down[j];
+            g_size[g_col[j]]--;
+        }
+    }
+}
+
+void restore(int col)
+{
+    /* 恢复col列标元素 */
+    g_left[g_right[col]] = g_right[g_left[col]] = col;
+    /* 恢复col列“1”元素与其同行元素 */
+    for (int i = g_up[col]; i != col; i = g_up[i])   // 恢复顺序与移除顺序相反
+    {
+        for (int j = g_left[i]; j != i; j = g_left[j])
+        {
+            g_up[g_down[j]] = g_down[g_up[j]] = j;
+            g_size[g_col[j]]++;
+        }
+    }
+}
+
+void link(int final[9][9])
+{
+    /* 链接列标元素 */
+    for (int i = 0; i < MAXCOL; i++)
+    {
+        g_left[i] = i - 1;
+        g_right[i] = i + 1;
+        g_up[i] = g_down[i] = i;
+        g_row[i] = 0;
+        g_col[i] = i;
+        g_size[i] = 0;
+    }
+    /* 链接Head元素 */
+    g_left[0] = MAXCOL - 1;
+    g_right[MAXCOL - 1] = 0;
+    /* 链接“1”元素 */
+    g_no = MAXCOL;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++) // 遍历9x9数独
+        {
+            int k = final[i][j];
+            if (k)
+            {
+                for (int t = 1; t <= 4; t++)  // 每个非0数字会在01矩阵中产生4个“1”元素
+                {
+                    g_left[g_no + t] = g_no + t - 1;
+                    g_right[g_no + t] = g_no + t + 1;
+                    g_row[g_no + t] = i * 81 + j * 9 + k - 1;
+                }
+                g_left[g_no + 1] = g_no + 4;
+                g_right[g_no + 4] = g_no + 1;
+                g_head[g_row[g_no + 1]] = g_no + 1;
+                /* 将4个元素插入列链中 */
+                insertNode(i * 9 + j + 1, g_no + 1);
+                insertNode(81 + i * 9 + k, g_no + 2);
+                insertNode(81 * 2 + j * 9 + k, g_no + 3);
+                insertNode(81 * 3 + (i / 3 * 3 + j / 3) * 9 + k, g_no + 4);
+                g_no += 4;
+            }
+            else  // 该位置为0，即待填
+            {
+                for (int k = 1; k <= 9; k++)        // 构造9种填法
+                {
+                    for (int t = 1; t <= 4; t++)    // 生成并链接它们的元素
+                    {
+                        g_left[g_no + t] = g_no + t - 1;
+                        g_right[g_no + t] = g_no + t + 1;
+                        g_row[g_no + t] = i * 81 + j * 9 + k - 1;
+                    }
+                    g_left[g_no + 1] = g_no + 4;
+                    g_right[g_no + 4] = g_no + 1;
+                    g_head[g_row[g_no + 1]] = g_no + 1;
+                    insertNode(i * 9 + j + 1, g_no + 1);
+                    insertNode(81 + i * 9 + k, g_no + 2);
+                    insertNode(81 * 2 + j * 9 + k, g_no + 3);
+                    insertNode(81 * 3 + (i / 3 * 3 + j / 3) * 9 + k, g_no + 4);
+                    g_no += 4;
+                }
+            }
+        }
+    }
+}
+
+bool findSolutions(int final[9][9], int select, int& solution)
+{
+    if (select > 81)
+    {
+        if (++solution == 1) // 已求出的解个数
+            return false;
+        throw(1);
+    }
+    /* 遍历列标元素，选一个元素最少的列（回溯率低） */
+    int col = 0;
+    int min = INT_MAX;
+    for (int i = g_right[0]; i != 0; i = g_right[i])
+    {
+        int size = g_size[i];
+        if (size == 0)
+            return false;
+        if (size < min) {
+            min = size;
+            col = i;
+        }
+    }
+    remove(col);
+    /* 遍历该列各“1”元素,逐行依次尝试,失败则恢复已移除的结点 */
+    for (int i = g_down[col]; i != col; i = g_down[i])
+    {
+        int row = g_row[i];
+        final[row / 81][row % 81 / 9] = row % 9 + 1;    // 根据该元素的行填入数独
+        for (int j = g_right[i]; j != i; j = g_right[j])    // 移除与该元素同行元素的列 
+            remove(g_col[j]);
+        if (findSolutions(final, select + 1, solution))    // 已选行数+1，递归
+            return true;
+        for (int j = g_left[i]; j != i; j = g_left[j])    // 返回false则恢复刚才的移除，顺序相反
+            restore(g_col[j]);
+    }
+    restore(col);
+    return false;
+}
+
+bool checkUnique(int final[9][9])
+{
+    init();
+    link(final);
+    int select = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            int k = final[i][j];
+            if (k)  // 代表数独题目已给数字的行必选
+            {
+                int row = i * 81 + j * 9 + k - 1;
+                select++;
+                remove(g_col[g_head[row]]);     // 移除该行首元素及其相关元素
+                for (int u = g_right[g_head[row]]; u != g_head[row]; u = g_right[u])
+                    remove(g_col[u]);         // 移除该行其他元素及其相关元素
+            }
+        }
+    }
+    int solution = 0;
+    try
+    {
+        findSolutions(final, select + 1, solution);
+        return true;
+    }
+    catch (int)
+    {
+        return false;
+    }
+}
+
 
 bool checkValid(int final[9][9], int row, int col, int& blanks)
 {
@@ -130,7 +333,6 @@ namespace SudokuTest
         void TestGenerate3()
         {
             Core c;
-            PuzzleSovlver ps;
             const int number = 100, lower = 40, upper = 55;
             int result[number][81];
             c.generate(number, lower, upper, true, result);
@@ -143,7 +345,7 @@ namespace SudokuTest
                 for (int j = 0; j < 81; j++)
                     Assert::IsTrue(checkValid(game, j / 9, j % 9, blanks), L"合法性出错");
                 Assert::IsTrue(blanks >= lower && blanks <= upper, L"挖空范围出错");
-                Assert::IsTrue(ps.checkUnique(game), L"唯一性出错");
+                Assert::IsTrue(checkUnique(game), L"唯一性出错");
             }
         };
 
@@ -196,7 +398,7 @@ namespace SudokuTest
                 c.generate(-1, 1, result);  // number传入-1
                 Assert::Fail(L"number范围出错");
             }
-            catch (NumberException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -205,7 +407,7 @@ namespace SudokuTest
                 c.generate(INT_MAX, 30, 40, true, result); // number传入最大int值
                 Assert::Fail(L"number范围出错");
             }
-            catch (NumberException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -221,7 +423,7 @@ namespace SudokuTest
                 c.generate(1, 0, result); // mode传入0
                 Assert::Fail(L"mode范围出错");
             }
-            catch (ModeException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -230,7 +432,7 @@ namespace SudokuTest
                 c.generate(1, 4, result); // mode传入4
                 Assert::Fail(L"mode范围出错");
             }
-            catch (ModeException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -246,7 +448,7 @@ namespace SudokuTest
                 c.generate(1, -1, 20, false, result); // lower传入-1
                 Assert::Fail(L"range范围出错");
             }
-            catch (RangeException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -255,7 +457,7 @@ namespace SudokuTest
                 c.generate(1, 50, 40, false, result); // 传入的lower比upper大
                 Assert::Fail(L"range范围出错");
             }
-            catch (RangeException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -264,7 +466,7 @@ namespace SudokuTest
                 c.generate(1, 20, 56, false, result); // upper传入56
                 Assert::Fail(L"range范围出错");
             }
-            catch (RangeException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             }
@@ -282,7 +484,7 @@ namespace SudokuTest
                 c.solve(result[0], result[0]); // 传入非法数独
                 Assert::Fail(L"解出非法数独");
             }
-            catch (ValidException& e)
+            catch (const std::exception& e)
             {
                 cout << e.what() << endl;
             };
